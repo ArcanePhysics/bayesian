b4498df60cbd3afceedd18cea6ca23ea000fe302
Snego <sneg@Mandarb.local>
2016-03-12 15:58:14 +0000

    first take at parsing document

---
+++ parsing/document.py
@@ -0,0 +1,67 @@
+import string
+
+class Word(object):
+	def __init__(self, text):
+		self.text = text
+		self.tag = None
+		self.frequency = 1
+		self.probability = 0
+
+	def __repr__(self):
+		return "%s (%s, %d, %f)" % (self.text, self.tag, self.frequency, self.probability)
+
+class Document(object):
+
+	def __init__(self):
+		self._rawData = []
+		self.words = {}
+
+	def parseFile(fileName):
+		f = open(fileName)
+		text = f.read().lower()
+		return Document._preprocessArray(text.split())
+
+	@staticmethod
+	def _preprocessWord(word):
+		preprocess = lambda w: w.strip(string.punctuation).lower()
+		return preprocess(word).strip()
+
+	@staticmethod
+	def _preprocessArray(stringArray):
+		r = []
+		for w in stringArray:
+			w = Document._preprocessWord(w)
+			if len(w) > 0:
+				r.append(w)
+		return r
+
+	def initFromFile(self, fileName):
+		self._rawData = parseFile(fileName)
+		self._initWords()
+
+	def initFromArray(self, stringArray):
+		self._rawData = Document._preprocessArray(stringArray)
+		self._initWords()
+
+	def _initWords(self):
+		self.words = {}
+		for w in self._rawData:
+			if w in self.words:
+				self.words[w].frequency = self.words[w].frequency + 1
+			else:
+				self.words[w] = Word(w)
+
+
+def test():
+	ar = ["hello", "World,", "Sup", "dude;", "232145", "", "   ", "gg", "hello", "Hello", "wOrlD"]
+	d = Document()
+	d.initFromArray(ar)
+	print d._rawData
+
+	## Example use case
+	for (key, obj) in d.words.iteritems():
+		print obj
+		# obj.probability <- get/set probability
+
+if __name__ == "__main__":
+ 	test()
