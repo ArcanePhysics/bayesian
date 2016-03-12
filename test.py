e1e7daed8f3a1eca630689295943160c7c7c46f8
mrlifetime@users.noreply.github.com <sneg.vx@gmail.com>
2016-03-12 17:54:10 +0000

    fix to test

--- test.py
+++ test.py
@@ -3,6 +3,19 @@ from TypeClass import Type
 from os import path
 
 def test():
+	a = ['a', 'bb', 'gg']
+	b = ['bb', 'c']
+
+	d1 = Document()
+	d2 = Document()
+	d1.initFromArray(a)
+	d2.initFromArray(b)
+
+	t = Type( d1, 'test' )
+	t.train(d2)
+
+	print t.words
+
 	test_dir = 'tests'
 	test_files = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10']
 
